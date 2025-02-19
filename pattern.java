17a028191137a5f81f5c8793f20f94288dd190af
Md Umar Faruk <mdumarfaruk793@gmail.com>
2025-02-19 20:23:00 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,15 @@
+public class pattern
+{
+public stayic void main(String[] args)
+{
+for(int i = 1; i<=4; i++)
+{
+for (int j = 4; j>=i; j--)
+{
+System.out.println("*");
+}
+
+System.out.println();
+}
+}
+}
